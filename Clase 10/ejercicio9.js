2657305441a92612a454bcf11c9f022589b705af
Ariadna Salomone <aricsalomone@gmail.com>
2025-06-04 00:12:48 -0300

    Subo ejercicio 9

---
+++ Clase 10/ejercicio9.js
@@ -0,0 +1,22 @@
+/*
+•	Ejercicio 9: Cálculo del IMC: Escribe un programa que calcule el Índice de Masa Corporal (IMC). El IMC se calcula con la fórmula: 
+
+ imc = peso / (altura * altura)
+
+Pide al usuario que ingrese su peso y su altura, y muestra el IMC calculado.
+Pista1: Investiga que es ParseFloat, ya que el ejercicio lo requiere
+Pista 2: La fórmula puede representarse asi:
+
+let imc = peso / (altura * altura);
+
+*/
+
+//para poder ingresar informacion por teclado
+const prompt = require('prompt-sync')();
+
+let peso = parseFloat(prompt("Ingrese su peso en kg: "));
+let altura = parseFloat(prompt("Ingrese su altura en m: "));
+
+let imc = peso/(altura * altura);
+
+console.log(`Si IMC es: ${imc}`);
